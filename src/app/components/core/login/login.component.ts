98eadc08dc96de303a4ca6864013547bb59a2598
Rhosung Park <rhosungpark@gmail.com>
2023-06-30 01:16:03 -0700

    adding verification input field

--- src/app/components/core/login/login.component.ts
+++ src/app/components/core/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  code = Array(6).fill(''); // Change 4 to the number of digits you want
+  @ViewChildren('inputs') inputs!: QueryList<ElementRef>;
 
+  onInputChange(event: Event, index: number) {
+    const input = event.target as HTMLInputElement;
+    this.code[index] = input.value;
+
+    if (input.value) {
+      const nextInput = this.inputs.toArray()[index + 1];
+      if (nextInput) {
+        nextInput.nativeElement.focus();
+      }
+    }
+  }
+
+  onKeydown(event: KeyboardEvent, index: number) {
+    if (event.key === 'Backspace' && this.code[index] === '') {
+      const previousInput = this.inputs.toArray()[index - 1];
+      if (previousInput) {
+        previousInput.nativeElement.focus();
+      }
+    }
+  }
 }
